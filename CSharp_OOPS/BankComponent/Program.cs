0645c615c9375b8cb933cc54ae6f7023c1c36bc6
girishkg95 <34678175+girishkg95@users.noreply.github.com>
2020-02-08 14:23:43 +0530

    Add files via upload

---
+++ CSharp_OOPS/BankComponent/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankComponent;
+
+namespace BankClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsBank objSB = new SavingsBank(35000.00);
+
+            Console.WriteLine("enter the principal amount");
+            var pr = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter the duration in years");
+            var duration = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter rate of interest (%)");
+            var rate = float.Parse(Console.ReadLine());
+
+            var si = objSB.GetSimpleInterest(pr, duration, rate);
+
+            Console.WriteLine("Simple Interst: Rs {0}", si);
+
+            var balance = objSB.GetBalance();
+
+            Console.WriteLine("Balance: Rs {0}", balance);
+
+            Console.ReadLine();
+        }
+    }
+}
